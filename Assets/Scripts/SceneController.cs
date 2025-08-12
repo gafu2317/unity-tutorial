1fe1f535d0468031fd90ab1217bd5373939c5162
gafu <0002317rynkmk@gmail.com>
2025-08-12 12:43:04 +0900

    UI設定

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -2,17 +2,46 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour
+
 {
-    // タイトルからメインへ
-    public void GoToMainScene()
-    {
-        SceneManager.LoadScene("Main");
-    }
-    
-    // 他のシーン遷移も追加可能
-    public void GoToTitleScene()
-    {
-        SceneManager.LoadScene("Title");
-    }
-    
+  // タイトルからメインへ
+  public void GoToMainScene()
+  {
+    SceneManager.LoadScene("Main");
+  }
+
+  // 他のシーン遷移も追加可能
+  public void GoToTitleScene()
+  {
+    SceneManager.LoadScene("Title");
+  }
+  public void GoToDungeonScene()
+  {
+    SceneManager.LoadScene("Dungeon");
+  }
+
+  public void GoToSettingsScene()
+  {
+    SceneManager.LoadScene("Settings");
+  }
+
+  public void GoToShopScene()
+  {
+    SceneManager.LoadScene("Shop");
+  }
+
+  public void GoToSpellCraftScene()
+  {
+    SceneManager.LoadScene("SpellCraft");
+  }
+
+  public void GoToGameOverScene()
+  {
+    SceneManager.LoadScene("GameOver");
+  }
+
+  public void GoToClearScene()
+  {
+    SceneManager.LoadScene("Clear");
+  }
 }
